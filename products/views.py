3dc650ed61ac1bf169204c7bd38505c0400466e8
Ridwan Yusuf <alabarise@gmail.com>
2020-08-26 05:36:33 +0100

    Redirect http://127.0.0.1:8000/query

--- products/views.py
+++ products/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect # redirect
 from django.db.models import Q
 from .models import * 
 # Create your views here.
 def query(request):
 	query = request.GET.get('query')
-	results = Product.objects.filter(
-            Q(name__icontains=query) | Q(price__icontains=query)
-            | Q(category__title__icontains=query) 
-        )
-	return render(request, 'search_results.html',
-     {  'results':results, })
+	if query is None:	# new
+		return redirect('home') # new
+	else:
+		results = Product.objects.filter(
+            Q(name__icontains=query) | Q(price__icontains=query)| Q(category__title__icontains=query))
+		return render(request, 'search_results.html',{ 'results':results, })
 
 def home(request):
 	products = Product.objects.all()
